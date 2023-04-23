802bfcc5b45c38455c0cfc70f2b5af1f7ef9580c
sjcineas <101082903+sjcineas@users.noreply.github.com>
2023-04-23 18:23:11 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from steam import Steam
 from decouple import config
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 
 KEY = config("STEAM_API_KEY")
@@ -10,6 +12,18 @@ steam = Steam(KEY)
 def get_user_by_name(user_name):
     data = steam.users.search_user(user_name)
     return data
+
+@st.cache_data
+def get_user_location(data):
+    try:
+        country = data['loccountrycode']
+        df = pd.read_csv("tableConvert.com_c47xbj.csv")
+        loc_data = df.loc[df["Alpha-2 code"] == country]
+        #st.success(loc_data)
+        loc_data = loc_data.rename(columns={'latitude': 'lat', 'longitude': 'lon'})
+        st.map(loc_data, zoom=2)
+    except:
+        st.warning("User's region couldn't be found")
 
 #HEADER
 st.title("Steam User Info")
@@ -29,10 +43,13 @@ if category == "Name":
             if user_data == "No match":
                 st.error("No Match Found")
             else:
-                st.success(user_data)
+                #st.success(user_data)
                 user_player = user_data["player"]
                 user_player_name = user_player["personaname"]
                 display_name = "User's Player Name: " + user_player_name
                 st.subheader(display_name)
+                loc_data = get_user_location(user_player)
+
+
 else:
     st.warning("Section Not Finished")
