2647f44317ebc02fadbd87530f1b518dc70f6dd5
CrisBlu <christianlav70@gmail.com>
2023-04-27 11:33:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
-from steam import Steam
 from decouple import config
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 
 api_key = "0121802C8A276CDC776E182910BCF0C5"
 KEY = config("STEAM_API_KEY")
-steam = Steam(KEY)
 
 #METHODS
 
@@ -33,19 +32,29 @@ def chart_content(tab_dict):
 @st.cache_data
 def findUser(url):
     id = url.replace("https://steamcommunity.com/profiles/", "")
-
+    
     if id == url:
         vanityid = url.replace("https://steamcommunity.com/id/", "")
+        vanityid = vanityid.replace('/', "")
         id = requests.get(
             f"https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={api_key}&vanityurl={vanityid}").json()
+        
+        if id["response"]["success"] != 1:
+            return 1
+        else:
+            id = id["response"]["steamid"]
 
     # Gets the raw JSON file
+    id = id.replace('/', "")
     data = requests.get(
         f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={api_key}&steamids={id}").json()
 
     # If there are no players in the response, return fail
     if data['response']['players'] == []:
         return 1
+    
+    if data['response']['players'][0]["communityvisibilitystate"] == 1:
+        return "private"
 
     # Return the first player in the response
     return data['response']['players'][0]
@@ -64,6 +73,7 @@ def findApp(id):
 
 @st.cache_data
 def getUserAchievements(user, app):
+   
     userID = user["steamid"]
     appID = app["appid"]
     userAch = requests.get(
@@ -71,8 +81,12 @@ def getUserAchievements(user, app):
 
     # Will fail if user has private achievements
     # Will fail if user has private achievements or 'achievements' key is missing
+    if not userAch["playerstats"]["success"]:
+        if userAch["playerstats"]["error"] == "Profile is not public":
+            return 'private'
+
     if not userAch["playerstats"]["success"] or "achievements" not in userAch["playerstats"]:
-        return 1
+            return 1
 
     return userAch["playerstats"]["achievements"]
 
@@ -92,7 +106,7 @@ def getOwnedGames(user, includeFree):
 
 @st.cache_data
 def total_ach(data):
-    count = 0;
+    count = 0
     if isinstance(data, list):
         for i in data:
             if int(i['achieved']) > 0:
@@ -107,24 +121,14 @@ def ach_record(user, gameLibrary):
         'Total Achievements': []
 
     }
+    
+    
     for i in gameLibrary:
         table['Game Name'].append(i['name'])
         table['Total Achievements'].append(total_ach(getUserAchievements(user, i)))
 
     return table
 
-@st.cache_data
-def get_user_by_name(user_name):
-    data = steam.users.search_user(user_name)
-    return data
-
-@st.cache_data
-def get_user_by_id(user_id):
-    try:
-        data = steam.users.get_user_details(user_id)
-        return data
-    except:
-        return st.error("An error occurred when retrieving User by Steam ID. Please enter a valid Steam ID.")
 
 @st.cache_data
 def get_user_location(data):
@@ -139,96 +143,74 @@ def get_user_location(data):
     except:
         st.warning("User's region couldn't be found")
 
+@st.cache_data
+def convertTime(data):
+    for i in data:
+        if i['achieved']:
+            unlockTime = i['unlocktime']
+            i['unlocktime'] = datetime.fromtimestamp(unlockTime).strftime('%Y/%m/%d %I:%M %p')
+        else:
+            i['unlocktime'] = '-'
+    return data
+
+@st.cache_data
+def getGlobalAch(app):
+    appID = app['appid']
+    globalAch = requests.get(f"https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/?key={api_key}&gameid={appID}").json()
+
+    return globalAch['achievementpercentages']['achievements']
+
+
 #HEADER
 st.title("Steam User Info")
 
-# Find User By
-find_by = ["Name", "User ID"]
-category = st.sidebar.selectbox("Find user by:", options= find_by)
 
-if category == "Name":
-    agreement = st.checkbox("By checking this box you agree to being aware that the information provided is not stolen. Any data displayed is provided by Steam.")
+agreement = st.checkbox("By checking this box you agree to being aware that the information provided is not stolen. Any data displayed is provided by Steam.")
 
-    if agreement:
-        user_name = st.text_input("Enter a username")
-        submit_user =st.button("Submit")
-        if submit_user:
-            user_data = get_user_by_name(user_name)
-            if user_data == "No match":
-                st.error("No Match Found")
+if agreement:
+    user_name = st.text_input("Enter a Steam Profile URL or a Steam Username")
+    #submit_user =st.button("Submit")
+    if user_name:
+        user = findUser(user_name)
+
+        if user != 1 and user != "private":
+            # Display Player found
+            st.success("Player Found")
+            user_player_name = user["personaname"]
+            st.subheader("User's Player Name: " + user['personaname'])
+            st.image(user['avatarfull'])
+               
+
+            loc_data = get_user_location(user)
+            gameLibrary = getOwnedGames(user, True)
+            
+            test = getUserAchievements(user, gameLibrary[0])
+            if test != 'private':
+                ach_for_player = ach_record(user, gameLibrary)
+
+                ach_df = pd.DataFrame(ach_for_player)
+                st.dataframe(ach_df)
+                chart_df = pd.DataFrame(chart_content(ach_for_player))
+                create_bar_chart(chart_df, 'Game Name', 'Total Achievements')
+                
+                gameOption = st.selectbox("Select Game", gameLibrary, index=0, format_func=lambda x: x['name'])
+                if gameOption:
+
+                    result = getUserAchievements(user, gameOption)
+                    if result == 1:
+                        st.error("No Achievements")
+                    else:
+                        globalResult = getGlobalAch(gameOption)
+                        globalGameAch = pd.DataFrame(globalResult)
+                        gameAch = pd.DataFrame(convertTime(result))
+
+                        st.dataframe(gameAch)
+                        st.dataframe(globalGameAch)
             else:
-                user = findUser(user_name)
-                #st.success(user_data)
-                user_player = user_data["player"]
-                user_player_name = user_player["personaname"]
-                #display_name = "User's Player Name: " + user_player_name
-                #st.subheader(display_name)
-                if user == 1:
-                    st.error("No Match Found")
-                else:
-
-                    # Display Player found
-                    st.success("Player Found")
-                    st.subheader("User's Player Name: " + user['personaname'])
-                    st.image(user['avatarfull'])
-                    gameLibrary = getOwnedGames(user, True)
-
-                    loc_data = get_user_location(user_player)
-                    gameLibrary = getOwnedGames(user, True)
-                    gameOption = gameLibrary[0]
-
-
-                    # st.subheader(getUserAchievements(user, gameOption))
-                    total_ach(getUserAchievements(user, gameOption))
-                    ach_for_player = ach_record(user, gameLibrary)
-
-                    ach_df = pd.DataFrame(ach_for_player)
-                    st.dataframe(ach_df)
-                    chart_df = pd.DataFrame(chart_content(ach_for_player))
-                    create_bar_chart(chart_df, 'Game Name', 'Total Achievements')
-
-
-
-
-else:
-    agreement = st.checkbox("By checking this box you agree to being aware that the information provided is not stolen. Any data displayed is provided by Steam.")
-    if agreement:
-        user_id = st.text_input("Enter a ID")
-        submit_user = st.button("Submit")
-        if submit_user:
-            user_data = get_user_by_id(user_id)
-            if user_data == "No match":
-                st.error("No Match Found")
-            else:
-                user_player = user_data["player"]
-                # st.success(user_data)
-                user_name = user_player['personaname']
-                user = findUser(user_name)
-                # st.success(user_data)
-
-                user_player_name = user_player["personaname"]
-                # display_name = "User's Player Name: " + user_player_name
-                # st.subheader(display_name)
-                if user == 1:
-                    st.error("No Match Found")
-                else:
-
-                    # Display Player found
-                    st.success("Player Found")
-                    st.subheader("User's Player Name: " + user['personaname'])
-                    st.image(user['avatarfull'])
-                    #gameLibrary = getOwnedGames(user, True)
-
-                    loc_data = get_user_location(user_player)
-                    gameLibrary = getOwnedGames(user, True)
-
-                    gameOption = st.selectbox("Select Game", gameLibrary, index=0, format_func=lambda x: x['name'])
-                    # st.subheader(getUserAchievements(user, gameOption))
-                    #total_ach(getUserAchievements(user, gameOption))
-                    ach_for_player = ach_record(user, gameLibrary)
-
-                    ach_df = pd.DataFrame(ach_for_player)
-                    st.dataframe(ach_df)
+                st.error("User Achievement Data is Private")
+                
+        else:
+            st.error("No Match Found")
 
 
 
